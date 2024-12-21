618865fed5bc9f23591b6d67b319bd0bdc17533e
Maksym Dzyubenko <ckraer@gmail.com>
2024-12-21 16:56:27 +0200

    Write in markup, styles and JS

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const categories = document.querySelectorAll('.item');
-console.log(`Number of categories: ${categories.length}`);
-categories.forEach(category => {
-    const categoryHeader = category.querySelector("h2");
-    const categoryItems = category.querySelectorAll("li");
-    console.log(`Category: ${categoryHeader.textContent}`);
-    console.log(`Elements: ${categoryItems.length}`);
-});
--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const nameInput = document.querySelector("#name-input");
-const nameOutput = document.querySelector("#name-output");
-
-const handleNameInput = e => {
-    nameOutput.textContent = e.target.value.trim() === "" ? "Anonymous" : e.target.value.trim();
-};
-
-nameInput.addEventListener("input", handleNameInput);
